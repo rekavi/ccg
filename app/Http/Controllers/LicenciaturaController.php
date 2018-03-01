9b36e99c4fcdfd0f294401bc367cb3ad0c5b7300
Renata Bonato <kavi@usp.br>
2018-03-01 15:32:13 -0300

    Controle licenciatura

---
+++ app/Http/Controllers/LicenciaturaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LicenciaturaController extends Controller
+{
+public function mostrarAlunos()
+{
+       return view('licenciatura.mostraralunos');
+}
+}
+    //
+
